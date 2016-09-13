1257505b355fe0c781609017594db19b7e1cc856
Mert Albayraktaroglu <albayraktaroglu@users.noreply.github.com>
2016-09-12 23:59:11 -0400

    Builder Pattern added!
    pizza builder example

---
+++ builderpattern/HawaiianPizzaBuilder.java
@@ -0,0 +1,9 @@
+package builderpattern;
+
+
+public class HawaiianPizzaBuilder extends PizzaBuilder {
+	  public void buildDough()   { pizza.setDough("cross"); }
+	  public void buildSauce()   { pizza.setSauce("mild"); }
+	  public void buildTopping() { pizza.setTopping("ham+pineapple"); }
+	  
+}
---
+++ builderpattern/Waiter.java
@@ -0,0 +1,15 @@
+package builderpattern;
+
+public class Waiter {
+	  private PizzaBuilder pizzaBuilder;
+
+	  public void setPizzaBuilder(PizzaBuilder pb) { pizzaBuilder = pb; }
+	  public Pizza getPizza() { return pizzaBuilder.getPizza(); }
+
+	  public void constructPizza() {
+	    pizzaBuilder.createNewPizzaProduct();
+	    pizzaBuilder.buildDough();
+	    pizzaBuilder.buildSauce();
+	    pizzaBuilder.buildTopping();
+	  }
+	}
